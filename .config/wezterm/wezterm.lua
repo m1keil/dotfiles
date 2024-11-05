2c634a0875adcf0917e2a5ae306184cf0b31cc83
Michael Sverdlik <m1keil@users.noreply.github.com>
2024-11-06 00:11:12 +1100

    update wezterm config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,33 +1,35 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local font = wezterm.font
 local action = wezterm.action
 local config = wezterm.config_builder()
 
 -- Fonts
-config.font = font('JetBrains Mono', { weight = 'Regular' })
+config.font = font("JetBrains Mono", { weight = "Regular" })
 config.font_size = 14.0
+config.adjust_window_size_when_changing_font_size = false
 
 -- Scrollback
 config.scrollback_lines = 10000
 
 -- Bell
-config.audible_bell = 'Disabled'
+config.audible_bell = "Disabled"
 
 -- UI
 config.enable_scroll_bar = true
-config.window_background_opacity = 0.85
+config.window_background_opacity = 1
 config.macos_window_background_blur = 20
-config.default_cursor_style = 'SteadyBlock'
+config.default_cursor_style = "SteadyBlock"
 config.initial_cols = 120
 config.initial_rows = 36
-config.window_decorations = 'RESIZE'
+config.window_decorations = "RESIZE"
 
+config.window_close_confirmation = "NeverPrompt"
 config.skip_close_confirmation_for_processes_named = {
-  'bash',
-  'sh',
-  'zsh',
-  'fish',
-  'tmux',
+	"bash",
+	"sh",
+	"zsh",
+	"fish",
+	"tmux",
 }
 
 -- Perf
@@ -38,43 +40,43 @@ config.automatically_reload_config = true
 
 -- Keys
 config.keys = {
-  {
-    key = 'p',
-    mods = 'CMD',
-    action = action.ActivateCommandPalette,
-  },
-  {
-    key = '|',
-    mods = 'CMD',
-    action = action.SplitHorizontal { domain = 'CurrentPaneDomain' },
-  },
-  {
-    key = '\\',
-    mods = 'CMD',
-    action = action.SplitVertical { domain = 'CurrentPaneDomain' },
-  },
-  {
-    key = 'w',
-    mods = 'CMD',
-    action = action.CloseCurrentPane { confirm = true },
-  },
-  {
-    key = '`',
-    mods = 'CMD',
-    action = action.PaneSelect {
-      alphabet = '1234567890',
-    }
-  },
-  { key = 'UpArrow', mods = 'CMD', action = action.ScrollToPrompt(-1) },
-  { key = 'DownArrow', mods = 'CMD', action = action.ScrollToPrompt(1) },
+	{
+		key = "p",
+		mods = "CMD",
+		action = action.ActivateCommandPalette,
+	},
+	{
+		key = "|",
+		mods = "CMD",
+		action = action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "\\",
+		mods = "CMD",
+		action = action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "w",
+		mods = "CMD",
+		action = action.CloseCurrentPane({ confirm = true }),
+	},
+	{
+		key = "`",
+		mods = "CMD",
+		action = action.PaneSelect({
+			alphabet = "1234567890",
+		}),
+	},
+	{ key = "UpArrow", mods = "CMD", action = action.ScrollToPrompt(-1) },
+	{ key = "DownArrow", mods = "CMD", action = action.ScrollToPrompt(1) },
 }
 
 config.mouse_bindings = {
-  {
-    event = { Down = { streak = 3, button = 'Left' } },
-    action = wezterm.action.SelectTextAtMouseCursor 'SemanticZone',
-    mods = 'NONE',
-  },
+	{
+		event = { Down = { streak = 3, button = "Left" } },
+		action = wezterm.action.SelectTextAtMouseCursor("SemanticZone"),
+		mods = "NONE",
+	},
 }
 
 return config
